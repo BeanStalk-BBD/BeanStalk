5ce2dcc495a6c708ed2b7b3d071b8f29eef29213
Lior-Becker <lior.becker@bbd.co.za>
2024-03-06 20:28:45 +0200

    created the server package

---
+++ Server/beanstalks/src/main/java/com/beanstalks/beanstalks/BeanstalksApplication.java
@@ -0,0 +1,13 @@
+package com.beanstalks.beanstalks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanstalksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanstalksApplication.class, args);
+	}
+
+}
---
+++ Server/beanstalks/src/test/java/com/beanstalks/beanstalks/BeanstalksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beanstalks.beanstalks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanstalksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
