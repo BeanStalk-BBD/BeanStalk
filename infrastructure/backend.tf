8324e353aa9927f8d4e7b600877e013182bc753d
Christo Kruger <christo.kruger@bbd>
2024-03-09 09:35:35 +0200

    added s3 bucket

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "doggroomingbucket"
+    bucket = "beanstalkbucket"
     key = "dog-grooming/terraform.tfstate"  # Specify the path/key for your state file
     region = "eu-west-1"
   }
