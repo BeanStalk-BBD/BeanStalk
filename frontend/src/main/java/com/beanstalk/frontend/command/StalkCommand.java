258e8efbfcdf555c72bc243df7af15b21b23ed27
Shane Theron BBD <Shane.Theron@bbd.co.za>
2024-03-14 00:46:28 +0200

    Formatted console output

--- frontend/src/main/java/com/beanstalk/frontend/command/StalkCommand.java
+++ frontend/src/main/java/com/beanstalk/frontend/command/StalkCommand.java
@@ -6,6 +6,8 @@ import java.util.List;
 import com.beanstalk.frontend.client.BeanStalkClient;
 import com.beanstalk.frontend.model.Message;
 import com.beanstalk.frontend.model.MessageResponse;
+import com.beanstalk.frontend.utils.Stalk;
+
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellMethodAvailability;
@@ -31,16 +33,29 @@ public class StalkCommand extends SecuredCommand {
 
     @ShellMethod("Sends a bean to the specified recipient and displays the relevant stalk.")
     @ShellMethodAvailability("isUserSignedIn")
-    public void bean(@ShellOption({"-R", "--recipient"}) String recipient, @ShellOption({"-M", "--message"}) String message) {
+    public String bean(@ShellOption({"-R", "--recipient"}) String recipient, @ShellOption({"-M", "--message"}) String message) {
         Message message_model = new Message(message, OffsetDateTime.now(), 0, userId);
         beanStalkClient.postMessage(recipient, message_model);
+        List<MessageResponse> messagesResponse = beanStalkClient.getMessages(recipient, userId);
+        return Stalk.CreateStalk(messagesResponse, recipient, userId); 
     }
 
     @ShellMethod("Displays the stalk for the specified recipient.")
     @ShellMethodAvailability("isUserSignedIn")
-    public List<MessageResponse> stalk(@ShellOption({"-R", "--recipient"}) String recieverName) { //@ShellOption({"-R", "--recipient"}) String recipient
-        List<MessageResponse> messagesResponse = beanStalkClient.getMessages(recieverName, userId);
-        return messagesResponse;
+    public String stalk(@ShellOption({"-R", "--recipient"}) String recipient) { 
+        List<MessageResponse> messagesResponse = beanStalkClient.getMessages(recipient, userId);
+        return Stalk.CreateStalk(messagesResponse, recipient, userId);
     }
+
+    // @ShellMethod("TEST")
+    // public String test(@ShellOption({"-R", "--recipient"}) String recipient) { 
+    //     // List<MessageResponse> messagesResponse = beanStalkClient.getMessages(recipient, userId);
+    //     List<MessageResponse> messagesResponse =  new ArrayList<MessageResponse>();
+    //     messagesResponse.add(new MessageResponse(4L, "wud", OffsetDateTime.now(), 1, 1));
+    //     messagesResponse.add(new MessageResponse(3L, "hi", OffsetDateTime.now(), 1, 2));
+    //     messagesResponse.add(new MessageResponse(2L, "oh", OffsetDateTime.now(), 1, 2));
+    //     messagesResponse.add(new MessageResponse(1L, "hi", OffsetDateTime.now(), 1, 1));
+    //     return Stalk.CreateStalk(messagesResponse, recipient, userId);
+    // }
 
 }
